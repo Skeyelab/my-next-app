f1e9b17e8c039f3fef33f6742839d95d86e778d4
Eric Dahl <edahl@nd.edu>
2025-02-12 15:19:51 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import localFont from "next/font/local";
-import { BubbleChat } from 'flowise-embed-react'
+import dynamic from 'next/dynamic';
+
+const BubbleChat = dynamic(
+  () => import('flowise-embed-react').then((mod) => mod.BubbleChat),
+  { ssr: false }
+);
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,19 +19,40 @@ const geistMono = localFont({
 
 export default function Home() {
   return (
-    <>
-      <div
-        className={`${geistSans.variable} ${geistMono.variable} min-h-screen flex items-center justify-center font-[family-name:var(--font-geist-sans)]`}
-      >
-        <main>
-          <h1>welcome</h1>
-
-        </main>
-      </div>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} min-h-screen flex items-center justify-center font-[family-name:var(--font-geist-sans)]`}
+    >
+      <main>
+        <h1>welcome</h1>
+      </main>
       <BubbleChat
         chatflowid="16e60cc4-38e8-43bd-bb4e-6361ebf72d77"
-        apiHost="https://flowise.ger.stereorail.com"
+        apiHost="https://flowise.ericdahl.dev"
+        theme={{
+          backgroundColor: "#000000",
+          textColor: "#ffffff",
+          buttonColor: "#000000",
+          buttonTextColor: "#ffffff",
+          chatWindow: {
+            showTitle: true,
+            showAgentMessages: true,
+            title: 'Hesburgh Library Chatbot',
+            welcomeMessage: 'Hi. My name is Ted. How can I help you today?',
+            errorMessage: 'This is a custom error message',
+            backgroundColor: '#ffffff',
+            backgroundImage: 'enter image path or link',
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            starterPrompts: [
+                "What resources are available for research?",
+                "How can you help me?"
+            ]
+          }
+        }}
       />
-    </>
+    </div>
+
+
   );
 }
